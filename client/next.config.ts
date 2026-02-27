641420ea99d8ca4b0f5d360ba9d902899c37edac
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-02-27 10:46:52 +0530

    client: configurations

---
+++ client/next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  turbopack: {},
+  webpack: (config) => {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
